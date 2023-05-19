cfef495e2a5b4ac57515b697ca79bbfd2f042a74
Felix Schmelzer <65311773+Felix2101@users.noreply.github.com>
2023-05-20 00:49:16 +0200

    removerd todo button

--- src/components/todo/TodoBox.tsx
+++ src/components/todo/TodoBox.tsx
@@ -1,26 +1,8 @@
 import { api } from "~/utils/api";
-import Button from "~/components/basic/Button";
-import { toast } from "react-toastify";
 import Todo from "~/components/todo/Todo";
 
 export function TodoBox() {
   const todos = api.todo.findAll.useQuery();
-
-  const createTodo = api.todo.create.useMutation({
-    onSuccess: async () => {
-      await todos.refetch();
-    },
-    onError: (err) => {
-      toast.error(err.message);
-    },
-  });
-
-  function createTodoHandler() {
-    createTodo.mutate({
-      title: "Neues Todo 🎉",
-      dueDate: new Date(),
-    });
-  }
 
   return (
     <div className="flex w-full flex-col gap-2">
@@ -29,7 +11,6 @@ export function TodoBox() {
           <Todo todo={todo} key={index} />
         ))}
       </div>
-      <Button onClick={createTodoHandler}>Create Todo</Button>
     </div>
   );
 }
