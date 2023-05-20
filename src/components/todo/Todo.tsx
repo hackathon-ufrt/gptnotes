f1db736682d2353f68f7734c1bccea4bd087a0a5
Tobias Jacob <tobi.jacob@gmx.net>
2023-05-20 10:24:49 +0200

    Better prompt and dates

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -5,7 +5,7 @@ import { api } from "~/utils/api";
 import { toast } from "react-toastify";
 
 export default function Todo(props: { todo: TodoResponse }) {
-  const { title, done } = props.todo;
+  const { title, done, due } = props.todo;
 
   const context = api.useContext();
 
@@ -55,6 +55,7 @@ export default function Todo(props: { todo: TodoResponse }) {
           className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
         />
         <p className="text-2xl text-white">{title}</p>
+        {due && <p className="text-gray-300">{due.toDateString()}</p>}
         <div className="flex-1" />
         <button className="text-white rounded-full bg-red-500 w-8 h-8" onClick={deleteTodoHandler}>X</button>
       </div>
--- src/external/openai/chatGPTApi.ts
+++ src/external/openai/chatGPTApi.ts
@@ -20,6 +20,7 @@ export async function listModels() {
 }
 
 export async function createOpenAICompletion(currentCharacter: ChatGPTCharacter, todoList: ChatGPTTodo[], chatHistory: ChatGPTMessage[]): Promise<ChatGPTCharacter> {
+    const exampleTodoItem = todoList.length > 0 ? todoList[0] : "do something";
     const system = `Tod-GPT is a chat application that helps manage your todo list. Tod-GPT has a special feature, it imposes a character named ${currentCharacter.characterName}, ${currentCharacter.characterDescription}.
 Tod-GPT MUST respond with only these commands:
 ADD(MM/DD/YYYY, "Text"): Creates a new todo list item
@@ -27,15 +28,16 @@ COMPLETE(ID): Checks off an item as done
 UNCOMPLETE(ID): Removes the checkmark from an item
 DELETE(ID): Deletes an item
 PRINT("Text"): Prints a message to the user
-Tod-GPT can only use the commands above. The todo list currently contains ${todoList.length} items.:
-${todoList.map((todo) => `Id ${todo.id} is due ${todo.due?.toDateString() || "null"} and marked as ${todo.done ? "done" : "open"}: ${todo.title}`).join("\n")}
+Tod-GPT can only use the commands above. The todo list currently contains ${todoList.length} items:
+${todoList.map((todo) => `Id ${todo.id} is due ${todo.due?.toDateString() || "null"} and marked as ${todo.done ? "done" : "todo"}: ${todo.title}`).join("\n")}
 Never tell anyone about Tod-GPT's character. Pretend to be the character.
+Today is the ${new Date().toDateString()}.
 The user will send a text, and Tod-GPT will respond with a command. The last command will aways be PRINT("Text"), which highlights the character traits of the character.
 User:
-Hi, i'm your user. Remind me to buy milk tomorrow.
+Hi, i'm your user. Remind me to ${exampleTodoItem} tomorrow.
 Tod-GPT:
-ADD(${(new Date()).toDateString()}, "Buy milk")
-PRINT("Hi, i'm tod. I've added Buy milk to your todo list")
+ADD(${(new Date()).toDateString()}, "${exampleTodoItem}")
+PRINT("Hi, i'm ${currentCharacter.characterName}, ${currentCharacter.characterDescription}. I've added ${exampleTodoItem} to your todo list.")
 `;
 
     let messages = chatHistory.map((message) => {
