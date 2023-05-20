a017b70cad850e375d11bb9a62b7df81fdeab9a8
Tobias Jacob <tobi.jacob@gmx.net>
2023-05-20 09:55:44 +0200

    Fix back button

--- src/components/chat/SelectPageWrapper.tsx
+++ src/components/chat/SelectPageWrapper.tsx
@@ -11,17 +11,15 @@ export function SelectPageWrapper() {
   return (
     <div className="flex w-full flex-col overflow-clip rounded bg-purple-200">
       <div className="h-10 w-full  rounded bg-white">
-        <button
+        {selected === "CHAT" && <button
           className="h-10 w-10"
           onClick={() => {
-            selected === "CHARACTER"
-              ? setSelected("CHAT")
-              : setSelected("CHARACTER");
+            setSelected("CHARACTER");
           }}
         >
           {"<"}
-        </button>
-        {me?.data?.activeCharacter?.name}
+        </button>}
+        {selected === "CHAT" && me?.data?.activeCharacter?.name}
       </div>
       {selected === "CHAT" ? (
         <ChatBox />
